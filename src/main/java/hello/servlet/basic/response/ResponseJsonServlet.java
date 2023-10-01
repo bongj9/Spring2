5e132c82f8b10cd34b1f3dae257c93938eb83313
bongj9 <lollypark501@naver.com>
2023-10-01 23:10:56 +0900

    주석처리

--- src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -26,6 +26,6 @@ public class ResponseJsonServlet extends HttpServlet {
         //{username:kim,age=20} ..이렇게 바꿀려면 objectmapper가 필요하다
         String result = objectMapper.writeValueAsString(helloData);
         resp.getWriter().write(result);
-
+       // HTTP 응답으로 JSON을 반환할 때는 content-type을 application/json 로 지정해야 한다. Jackson 라이브러리가 제공하는 objectMapper.writeValueAsString() 를 사용하면 객체를 JSON
     }
 }
