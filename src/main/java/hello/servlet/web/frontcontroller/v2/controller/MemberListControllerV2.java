f786e38642b4b4ae458a74ce22bcb4f6494433d9
bongj9 <lollypark501@naver.com>
2023-10-13 23:58:36 +0900

    controllerV3

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -18,7 +18,7 @@ public class MemberListControllerV2 implements ControllerV2 {
     public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Member> members = memberRepository.findAll();
 
-        request.setAttribute("members", members); //members와 member의 차이가 중요
+        request.setAttribute("members", members); //members와 member의 차이가 중
 
         return new MyView("/WEB-INF/views/members.jsp");
 
