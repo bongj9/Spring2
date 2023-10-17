af860e53b2bc116d4deee5c701e2441d41543738
bongj9 <lollypark501@naver.com>
2023-10-17 18:08:34 +0900

    들여쓰기

--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -27,11 +27,13 @@ public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
         return mv;
 
     }
-    private static Map<String, String> createParamMap(HttpServletRequest request) {
+    private Map<String, String> createParamMap(HttpServletRequest request) {
         Map<String, String> paramMap = new HashMap<>();
         request.getParameterNames().asIterator()
-                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+                .forEachRemaining(paramName -> paramMap.put(paramName,
+                        request.getParameter(paramName)));
         return paramMap;
+
     }
 
 
