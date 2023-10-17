167d81b24f4ff8db3c225f0e93679102b29102fc
bongj9 <lollypark501@naver.com>
2023-10-17 17:29:37 +0900

    Adapter

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,73 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+    private final Map<String, Object> handlerMappingMap =new HashMap<>();//모든 Controller버전를을 다 쓰기위해 Object로 만들었다 최상위객체인 Object
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+        initHandlerAdapters();
+      }
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+    private void initHandlerAdapters() {
+
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Object handler = getHandler(request);
+        if (handler == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        } //다형성에 의해서 인스턴스로 받을 수 있다.
+        MyHandlerAdapter adapter = getHandlerAdapter(handler); //handler를 가져왔음
+        ModelView mv = adapter.handle(request, response, handler); //실제 컨ㄷ트롤러를 호출하고 모델뷰를 반
+
+        MyView view = viewResolver(mv.getViewName());
+        view.render(mv.getModel(),request,response);
+    }
+
+    private  MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) { //반복루프를 이용해 어뎁터를 찾아야한다. iter을 입력하면 for eacha문으로 쉽게 만들 수 있다.
+            if (adapter.supports(handler)) {
+                 return adapter;
+            }
+
+        }
+        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다. handler :" +handler);
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURL = request.getRequestURI();
+        return handlerMappingMap.get(requestURL);
+    }
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+}
